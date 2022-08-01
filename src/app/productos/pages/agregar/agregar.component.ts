31ae13c0d6b5a551bcda30ae716998c599fd223b
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-08-01 13:54:10 +0200

    Directivas personalizadas

---
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styles: [
+  ]
+})
+export class AgregarComponent {
+
+  texto1: string = 'Fernando Herrera';
+  color: string = 'green';
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', Validators.required ]
+  });
+
+  constructor( private fb: FormBuilder ) { }
+
+  tieneError( campo: string ): boolean {
+    return this.miFormulario.get( campo )?.invalid || false;
+  }
+
+  cambiarNombre() {
+    this.texto1 = Math.random().toString();
+  }
+
+  cambiarColor() {
+    this.color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+  }
+
+}
